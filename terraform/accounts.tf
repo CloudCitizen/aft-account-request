ed0f2b0f149636343509177d16e710c7747e265a
Alex Galani <alex@cloudcitizen.eu>
2022-09-08 14:13:16 +0200

    add deployment account

--- terraform/accounts.tf
+++ terraform/accounts.tf
@@ -19,3 +19,43 @@ module "polaris_audit" {
 
   account_customizations_name = "audit-customizations"
 }
+
+module "polaris_logging" {
+  source = "./modules/aft-account-request"
+  control_tower_parameters = {
+    AccountEmail              = "untaxed-elision0g@icloud.com"
+    AccountName               = "Log Archive"
+    ManagedOrganizationalUnit = "Security"
+    SSOUserEmail              = "galani@me.com"
+    SSOUserFirstName          = "Alex"
+    SSOUserLastName           = "Galani"
+  }
+  account_tags = {
+    "Owner" = "Platform Team"
+  }
+  account_customizations_name = "logging-customizations"
+}
+
+module "aft_deploy_account" {
+  source = "./modules/aft-account-request"
+  control_tower_parameters = {
+    AccountEmail              = "galani+aft@me.com"
+    AccountName               = "AFT-Management"
+    ManagedOrganizationalUnit = "deploy"
+    SSOUserEmail              = "galani@me.com"
+    SSOUserFirstName          = "Alex"
+    SSOUserLastName           = "Galani"
+  }
+}
+
+module "polaris_runtime_dev" {
+  source = "./modules/aft-account-request"
+  control_tower_parameters = {
+    AccountEmail              = "galani+dev@me.com"
+    AccountName               = "runtime-dev"
+    ManagedOrganizationalUnit = "NonSensitive"
+    SSOUserEmail              = "galani@me.com"
+    SSOUserFirstName          = "Alex"
+    SSOUserLastName           = "Galani"
+  }
+}
